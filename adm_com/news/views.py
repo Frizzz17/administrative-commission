c67a5ee63aca7faaf6717e4f601bea777955769a
Андрей Фризен <frizen_a@inbox.ru>
2023-01-18 15:08:58 +0700

    Сделал модель news и админку

--- adm_com/news/views.py
+++ adm_com/news/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from news.models import News
 
 
 def index(request):
     template = 'news/index.html'
+    articles_list = News.objects.all()
+
+    context = {
+        'articles_list': articles_list
+    }
+    
+
     return render(request, template)
