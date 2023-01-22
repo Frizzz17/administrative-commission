ef00467ecca4e5872ae8227de2d4bddc625f1bee
Андрей Фризен <frizen_a@inbox.ru>
2023-01-22 17:25:37 +0700

    Подключил новую базу данных

--- adm_com/adm_com/settings.py
+++ adm_com/adm_com/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'adm_com.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adm_com',
+        'USER': 'postgres',
+        'PASSWORD': '24demagu',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
