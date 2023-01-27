ca783d154713390250d1796b21e3163fa32f8e99
Андрей Фризен <frizen_a@inbox.ru>
2023-01-27 10:41:44 +0700

    Добавил проверку аутентификации пользователя для создания нового пользователя

--- adm_com/users/views.py
+++ adm_com/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -6,6 +6,21 @@ from users.forms import CreateUserForm
 
 
 class NewUser(CreateView):
+    '''Регестрирует нового пользователя.'''
     form_class = CreateUserForm
     success_url = reverse_lazy('news:index')
     template_name = 'users/reg_user.html'
+    
+    def get(self, request, *args, **kwargs):
+        '''Проверяет является ли пользователь администратором и отдает get. '''
+        self.object = None
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        return redirect('users:login')
+
+    def post(self, request, *args, **kwargs):
+        '''Проверяет является ли пользователь администратором и отдает post. '''
+        self.object = None
+        if request.request.is_autenticated:
+            return super().post(request, *args, **kwargs)
+        return redirect('users:login')
