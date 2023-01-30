ab504462566225499d3971c2281bd9bd84499c70
Андрей Фризен <frizen_a@inbox.ru>
2023-01-30 21:45:20 +0700

    Добавил модель ролей пользователей, и зарегестрировал их в админке

--- adm_com/users/admin.py
+++ adm_com/users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from users.models import User
+from users.models import User, Role, RoleUser
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Role)
+admin.site.register(RoleUser)
--- adm_com/users/models.py
+++ adm_com/users/models.py
@@ -6,3 +6,20 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     middle_name = models.CharField(_("Отчество"), max_length=150, blank=True)
     position = models.CharField(_("Должность"), max_length=300, blank=True)
+
+
+class Role(models.Model):
+    title = models.CharField(
+        max_length=200, 
+        verbose_name='Роль в административной комиссии'
+    )
+    user = models.ManyToManyField(User, through='RoleUser')
+
+    def __str__(self):
+        return self.title
+
+
+class RoleUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
