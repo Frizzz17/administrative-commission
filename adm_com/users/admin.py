26f4ee85e23fb6f2c4f480d0909507d22357e3a5
Андрей Фризен <frizen_a@inbox.ru>
2023-02-08 10:22:51 +0700

    Внёс изменения в админку приложения users

--- adm_com/users/admin.py
+++ adm_com/users/admin.py
@@ -4,6 +4,10 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import User, Role, RoleUser
 
 
+@admin.register(RoleUser)
+class RoleUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'is_admin')
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Role)
-admin.site.register(RoleUser)
