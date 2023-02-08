a68e87cf42caac91499e7bad80983f2be07c1a6d
Андрей Фризен <frizen_a@inbox.ru>
2023-02-08 09:13:32 +0700

    Изменил БД на sqlite

--- adm_com/adm_com/settings.py
+++ adm_com/adm_com/settings.py
@@ -69,12 +69,8 @@ WSGI_APPLICATION = 'adm_com.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'adm_com',
-        'USER': 'postgres',
-        'PASSWORD': '24demagu',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
