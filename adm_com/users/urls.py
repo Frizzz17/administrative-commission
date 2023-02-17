3d7eab53d1ea165a4b74baa0bbfd5a8b0b0bdd7d
Андрей Фризен <frizen_a@inbox.ru>
2023-02-17 16:21:00 +0700

    Добавил шаблон инормации о пользователе

--- adm_com/users/urls.py
+++ adm_com/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView, LoginView
-from users.views import NewUser, role_for_user, del_role_for_user
+from users import views
 
 
 app_name = 'users'
@@ -12,12 +12,18 @@ urlpatterns = [
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
-    path('new_user/', NewUser.as_view(), name='new_user'),
+    path('new_user/', views.NewUser.as_view(), name='new_user'),
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
-    path('roles/<str:username>/', role_for_user, name='roles'),
-    path('roles/del_role/<str:username>', del_role_for_user, name='del_role')
+    path('roles/<str:username>/', views.role_for_user, name='roles'),
+    path(
+        'roles/del_role/<str:username>',
+        views.del_role_for_user,
+        name='del_role'
+    ),
+    path('all_users/', views.get_all_users, name='all_users'),
+    path('user/<str:username>/', views.user_info, name='userinfo')
 ]
--- adm_com/users/views.py
+++ adm_com/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -8,9 +8,9 @@ from users.models import User
 
 
 class NewUser(CreateView):
-    '''Регестрирует нового пользователя.'''
+    '''Регистрирует нового пользователя.'''
     form_class = CreateUserForm
-    success_url = reverse_lazy('users:roles', kwargs={'username': 'andr'})
+    # success_url = reverse_lazy('users:roles', kwargs={'username': username}) Переделать на редирект таблицы пользователей
     template_name = 'users/reg_user.html'
     
     def get(self, request, *args, **kwargs):
@@ -26,6 +26,7 @@ class NewUser(CreateView):
         if request.user.is_authenticated:
             return super().post(request, *args, **kwargs)
         return redirect('users:login')
+
 
 roles = []
 @login_required
@@ -55,3 +56,29 @@ def del_role_for_user(request, username):
     roles.pop()
     print(roles)
     return redirect('users:roles', username)
+
+
+@login_required
+def get_all_users(request):
+    '''Получает всех пользователей.'''
+    users = User.objects.all()
+    template = 'users/all_users.html'
+
+    context = {
+        'users': users,
+    }
+
+    return render(request, context=context, template_name=template)
+
+
+@login_required
+def user_info(request, username):
+    user = get_object_or_404(User, username=username)
+
+    template = 'users/user_info.html'
+
+    context = {
+        'user': user
+    }
+
+    return render(request, context=context, template_name=template)
